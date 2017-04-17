b3d013c3952a47fa040aa18216433dd2acc1957a
SystemInterrupt <vladislavtretyal@bk.ru>
2017-04-17 20:39:31 +0300

    Commit

--- src/main/java/com/springapp/mvc/dao/implementions/AxesDAOImpl.java
+++ src/main/java/com/springapp/mvc/dao/implementions/AxesDAOImpl.java
@@ -20,31 +20,5 @@ public class AxesDAOImpl implements AxesDAO {
     public List<String> getAxes() {
         return sessionFactory.getCurrentSession().createQuery("select axes from AxesFilter ").list();
     }
-//
-//
-//    @Override
-//    @SuppressWarnings("unchecked")
-//    public List<String> getReachValues() {
-//        return sessionFactory.getCurrentSession().createQuery("select reachValues from SlidersFilter order by reachValues").list();
-//    }
-//
-//    @SuppressWarnings("unchecked")
-//    public List<SlidersFilter> getAllElements(){
-//        return sessionFactory.getCurrentSession().createCriteria(SlidersFilter.class).list();
-//    }
-//
-//    @Override
-//    public void uploadAxesFilter(AxesFilter axesFilter) {
-//        sessionFactory.getCurrentSession().saveOrUpdate(axesFilter);
-//    }
-//
-//    @Override
-//    public void addAxesFilter(AxesFilter axesFilter) {
-//        sessionFactory.getCurrentSession().save(axesFilter);
-//    }
-//
-//    @Override
-//    public void deleteAxesFilter(AxesFilter axesFilter) {
-//        sessionFactory.getCurrentSession().delete(axesFilter);
-//    }
+
 }
